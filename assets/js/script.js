c3300e1b1724d57a500b06aecdafcc3d54b33531
Wendy Wilgus <wilguswendy@gmail.com>
2022-12-20 12:58:10 -0500

    score functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,34 @@ var wordBlank = document.querySelector(".word-blanks");
 var chosenWord = "";
 var numBlanks = 0;
 
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+var timer;
 var timerCount;
+var isWin = false;
+var win = document.querySelector(".win");
+var loss = document.querySelector(".losses");
+var winCounter = 0;
+var loseCounter = 0;
 
+function init() {
+    getWins();
+    getLosses();
+}
 
+function startGame() {
+    isWin = false;
+    timerCount = 30;
+    startButton.disabled = true;
+    renderBlanks();
+    startTimer();
+}
+
+function checkWin() {
+    if (chosenWord === blanks.join("")) {
+        isWin = true;
+    }
+}
 
 function renderBlanks() {
     chosenWord = words[Math.floor(Math.random() * words.length)];
@@ -53,6 +78,67 @@ document.addEventListener("keydown", function(event) {
         checkWin();
     }
 });
+
+function winGame() {
+    wordBlank.textContent = "You Won! 🏆";
+    winCounter++
+    startButton.disabled = false;
+    setWins();
+}
+
+function loseGame() {
+    wordBlank.textContent = "Game Over";
+    loseCounter++
+    startButton.disabled = false;
+    setLosses()
+}
+
+function setWins() {
+    win.textContent = winCounter;
+    localStorage.setItem("winCount", winCounter);
+}
+
+function setLosses() {
+    loss.textContent = loseCounter;
+    localStorage.setItem("loseCount", loseCounter);
+}
+
+function getWins() {
+    let storedWins = localStorage.getItem("winCount");
+    if (storedWins === null) {
+        winCounter = 0;
+    } else {
+        winCounter = storedWins;
+    }
+    win.textContent = winCounter;
+}
+
+function getLosses() {
+    let storedLosses = localStorage.getItem("loseCount");
+    if (storedLosses === null) {
+        loseCounter = 0;
+    } else {
+        loseCounter = storedLosses;
+    }
+    loss.textContent = loseCounter;
+}
+
+function startTimer() {
+    timer = setInterval(function() {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            if (isWin && timerCount > 0) {
+            clearInterval(timer);
+            winGame();
+        }
+    }
+    if (timerCount === 0) {
+        clearInterval(timer);
+        loseGame();
+    }
+}, 1000);
+}
 
 startButton.addEventListener("click", startGame);
 
